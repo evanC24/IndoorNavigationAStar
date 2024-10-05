8fdfe958b2149d64fcbc49e2cdb930ef69619dcd
evanC24 <coppolaivan01@gmail.com>
2024-10-05 10:53:28 +0200

    Initial Commit

---
+++ IndoorNavigationAStar/IndoorNavigationAStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndoorNavigationAStarApp.swift
+//  IndoorNavigationAStar
+//
+//  Created by Ivan Coppola on 05/10/24.
+//
+
+import SwiftUI
+
+@main
+struct IndoorNavigationAStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
